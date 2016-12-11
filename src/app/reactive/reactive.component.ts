5aa9a02abaa7ffce29397aa38cf5637b47b0e598
WillyLu326 <luzh0326@gmail.com>
2016-12-10 23:16:52 -0500

    Reactive Form Validation - FormGroup FormBuilder Validators

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -12,6 +12,22 @@ export class ReactiveComponent implements OnInit {
 	nameErrorMsg: string;
 	usernameErrorMsg: string;
     re: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    errorFields: Object = {
+    	name: '',
+    	username: ''
+    };
+    errorMsgs: Object = {
+    	name: {
+    		required: 'Name is required',
+    		minlength: 'Name length is at least 3 characters',
+    		maxlength: 'Name length can\'t longer than 6 characters'
+    	},
+    	username: {
+    		required: 'Username is required',
+    		pattern: 'Username should be email'
+    	}
+    }
 
   	constructor(private fb: FormBuilder) { }
 
@@ -23,52 +39,34 @@ export class ReactiveComponent implements OnInit {
   		// });
 
   		// Using FormBuilder to create form
+  		this.buildForm();
+  	}
+
+  	buildForm() {
   		this.form = this.fb.group({
-  			name: ['', 
-  				   [ 
-  				   	 	Validators.minLength(3), 
-  				   	 	Validators.maxLength(6)
-  				   ]
-  				],
-  			username: ['', 
-  					[ 
-  						Validators.pattern(this.re)
-  					]
-  				]
+  			name: ['', [ Validators.minLength(3), Validators.maxLength(6)]],
+  			username: ['', [ Validators.pattern(this.re)]]
   		})
   	
   		// watch changes and validate
-  		this.form.valueChanges.subscribe( data => {
-  			this.nameErrorMsg = '';
-  			this.usernameErrorMsg = '';
-  			
-  			let name = this.form.get('name');
-  			let username = this.form.get('username');
+  		this.form.valueChanges.subscribe( data => this.validateForm());
+  	}
 
-  			if (name.invalid && name.touched) {
-  				if (name.errors['required']) {
-  					this.nameErrorMsg = 'Name is required';
-  				}
-  				
-  				if (name.errors['minlength']) {
-  					this.nameErrorMsg = 'Name length is at least 3 characters.';
-  				}
-
-  				if (name.errors['maxlength']) {
-  					this.nameErrorMsg = 'Name length cant\'t more than 6 characters.';
-  				}
-  			}
-
-  			if (username.invalid && username.touched) {
-  				if (username.errors['required']) {
-  					this.usernameErrorMsg = 'Username is required.';
-  				}
-
-  				if (username.errors['pattern']) {
-  					this.usernameErrorMsg = 'Username should be email';
-  				}
-  			}
-  		})
+  	validateForm() {
+		for (let field in this.errorFields) {
+			// clear error message firstly
+			this.errorFields[field] = '';
+			// grab input field by name
+			let input = this.form.get(field);
+			
+			if (input.invalid && input.dirty) {
+				// loop errors of input
+				for(let error in input.errors) {
+					// assign error message into fieldMessages
+					this.errorFields[field] = this.errorMsgs[field][error];
+				}
+			}
+		}	
   	}
 
   	processData() {
