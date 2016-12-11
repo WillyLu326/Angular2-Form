e28b68c4bbdc789b48817117312e9761ad9286f0
WillyLu326 <luzh0326@gmail.com>
2016-12-10 21:01:46 -0500

    Reactive Form Validation - Validators.Errors

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validator } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -9,6 +9,9 @@ import { FormGroup, FormBuilder, Validator } from '@angular/forms';
 export class ReactiveComponent implements OnInit {
 
 	form: FormGroup;
+	nameErrorMsg: string;
+	usernameErrorMsg: string;
+    re: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   	constructor(private fb: FormBuilder) { }
 
@@ -21,10 +24,51 @@ export class ReactiveComponent implements OnInit {
 
   		// Using FormBuilder to create form
   		this.form = this.fb.group({
-  			name: '',
-  			username: ''
+  			name: ['', 
+  				   [ 
+  				   	 	Validators.minLength(3), 
+  				   	 	Validators.maxLength(6)
+  				   ]
+  				],
+  			username: ['', 
+  					[ 
+  						Validators.pattern(this.re)
+  					]
+  				]
   		})
-  		console.log(this.form);
+  	
+  		// watch changes and validate
+  		this.form.valueChanges.subscribe( data => {
+  			this.nameErrorMsg = '';
+  			this.usernameErrorMsg = '';
+  			
+  			let name = this.form.get('name');
+  			let username = this.form.get('username');
+
+  			if (name.invalid && name.touched) {
+  				if (name.errors['required']) {
+  					this.nameErrorMsg = 'Name is required';
+  				}
+  				
+  				if (name.errors['minlength']) {
+  					this.nameErrorMsg = 'Name length is at least 3 characters.';
+  				}
+
+  				if (name.errors['maxlength']) {
+  					this.nameErrorMsg = 'Name length cant\'t more than 6 characters.';
+  				}
+  			}
+
+  			if (username.invalid && username.touched) {
+  				if (username.errors['required']) {
+  					this.usernameErrorMsg = 'Username is required.';
+  				}
+
+  				if (username.errors['pattern']) {
+  					this.usernameErrorMsg = 'Username should be email';
+  				}
+  			}
+  		})
   	}
 
   	processData() {
